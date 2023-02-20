047280aa03c3973da5c8bad3ac68299372e7804b
Nathan Youngman <git@nathany.com>
2023-02-20 09:53:34 -0700

    simplify main

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@
 /// Inspired by Four Thousand Weeks by Oliver Burkeman.
 use weeks::{format_local_date_time, now, parse_date_time, Person, Pronoun};
 
-// NOTE: -08 is PST. Daylight Saving Time started in B.C. on Sunday, April 24, 1977.
-const BIRTHDATE: &str = "1977-04-05 11:58 -08";
-
 fn main() {
     let now = now();
-    let birthdate = parse_date_time(BIRTHDATE).unwrap();
+
+    // NOTE: -08 is PST. Daylight Saving Time started in B.C. on Sunday, April 24, 1977.
+    let birthdate = parse_date_time("1977-04-05 11:58 -08").unwrap();
     let person = Person::new("Nathan", Pronoun::HeHim, birthdate, "British Columbia");
 
     println!("Current time is {}\n", format_local_date_time(now));
 
-    println!("Born on {}.", person.birth());
+    println!("{} was born on {}.", person.name, person.birth());
     println!("Alive for {}.", person.age(now));
 }
