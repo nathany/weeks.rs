467a1af8620985fcbe34dcbb731849a3fecfcbbb
Nathan Youngman <git@nathany.com>
2024-09-18 18:19:37 -0600

    change integration test to unit test

--- src/lib.rs
+++ src/lib.rs
@@ -126,3 +126,27 @@ impl Age {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn age() {
+        let duration = chrono::Duration::weeks(107)
+            + chrono::Duration::days(5)
+            + chrono::Duration::hours(13)
+            + chrono::Duration::minutes(38);
+        let age = Age::new(duration);
+
+        assert_eq!(age.weeks, 107);
+        assert_eq!(age.days, 5);
+        assert_eq!(age.hours, 13);
+        assert_eq!(age.minutes, 38);
+
+        assert_eq!(
+            format!("{}", age),
+            "107 weeks, 5 days, 13 hours, and 38 minutes"
+        )
+    }
+}
